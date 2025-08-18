65281c1692558df0a439761e456334ef6e851e88
Francesco Saccone <francesco@francescosaccone.com>
2025-08-18 20:29:03 +0200

    build(sys): use cross ar instead of native

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@ ARCH = riscv
 
 CROSS_COMPILE = $(ARCH)-none-elf-
 
-AR      = ar
+AR      = $(CROSS_COMPILE)ar
 AS      = $(CROSS_COMPILE)as
 CC      = $(CROSS_COMPILE)cc
 LD      = $(CROSS_COMPILE)ld
