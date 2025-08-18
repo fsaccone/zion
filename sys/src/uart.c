e22e8de351ef4c3ec55bf6ff87479309bc4d57b6
Francesco Saccone <francesco@francescosaccone.com>
2025-08-18 12:30:25 +0200

    feat(sys): define uart functions in uart.c

---
+++ sys/src/uart.c
@@ -0,0 +1,14 @@
+#include <uart.h>
+
+void
+uart_print(const char *str)
+{
+	while (*str) uart_putc(*str++);
+}
+
+void
+uart_putc(char c)
+{
+	while(!(*UART_LSR & UART_LSR_EMPTY_MASK));
+	*UART_THR = c;
+}
