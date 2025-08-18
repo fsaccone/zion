1c871b88046cda2f5acd1f9adec2e78a7e348dc2
Francesco Saccone <francesco@francescosaccone.com>
2025-08-18 13:25:00 +0200

    fix(riscv): put all non-o harts in infinite loop

--- sys/arch/riscv/src/boot.s
+++ sys/arch/riscv/src/boot.s
@@ -1,8 +1,15 @@
 .section .text
 .globl _start
 _start:
+	li t0, 0         # Hart ID to use
+	csrr t1, mhartid # Hart ID in use
+	bne t0, t1, loop # Disable non-0 harts
+
 	la sp, stacktop
 	j kmain
+
+loop:
+	j loop
 
 .section .bss
 .align 12
