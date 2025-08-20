568fe270650d6a62eb1c9f1105619fcb34595ccd
Francesco Saccone <francesco@francescosaccone.com>
2025-08-20 12:02:33 +0200

    refactor(sys/riscv): rename loop symbol to lock

--- sys/arch/riscv/boot/start.s
+++ sys/arch/riscv/boot/start.s
@@ -3,11 +3,11 @@
 _start:
 	li   t0, 0        # Hart ID to use
 	csrr t1, mhartid  # Hart ID in use
-	bne  t0, t1, loop # Disable non-0 harts
+	bne  t0, t1, lock # Lock non-0 harts
 
 	la sp, _stack_end
 	j  kmain
 
-loop:
+lock:
 	wfi
-	j loop
+	j lock
