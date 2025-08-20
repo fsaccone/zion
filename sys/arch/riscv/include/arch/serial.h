8d15d3db06c8225452051e00cf7295ebe3b76b76
Francesco Saccone <francesco@francescosaccone.com>
2025-08-20 19:46:48 +0200

    refactor(sys/riscv): move UART macros from h file to c file

--- sys/arch/riscv/include/arch/serial.h
+++ sys/arch/riscv/include/arch/serial.h
@@ -1,13 +1,6 @@
 #ifndef _ARCH_SERIAL_H
 #define _ARCH_SERIAL_H
 
-#include <arch/types.h>
-
-#define UART0               (uint8_t *)(0x10000000)
-#define UART_THR            (uint8_t *)(UART0)
-#define UART_LSR            (uint8_t *)(UART0 + 0x05)
-#define UART_LSR_EMPTY_MASK (0x40)
-
 void serial_print(const char *str);
 
 #endif
--- sys/arch/riscv/lib/serial.c
+++ sys/arch/riscv/lib/serial.c
@@ -1,5 +1,12 @@
 #include <arch/serial.h>
 
+#include <arch/types.h>
+
+#define UART0               (uint8_t *)(0x10000000)
+#define UART_THR            (uint8_t *)(UART0)
+#define UART_LSR            (uint8_t *)(UART0 + 0x05)
+#define UART_LSR_EMPTY_MASK (0x40)
+
 void
 serial_print(const char *str)
 {
