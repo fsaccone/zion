f2269208e91b7f32b3e2b16ab395d59c2e2e568c
Francesco Saccone <francesco@francescosaccone.com>
2025-08-20 15:17:56 +0200

    refactor(sys/riscv): modularize creating lock_harts label

--- sys/arch/riscv/boot/start.s
+++ sys/arch/riscv/boot/start.s
@@ -1,12 +1,17 @@
 .section .entry.text
 .globl _start
 _start:
-	li   t0, 0        # Hart ID to use
-	csrr t1, mhartid  # Hart ID in use
-	bne  t0, t1, lock # Lock non-0 harts
+	call lock_harts
 
 	la sp, _stack_end
 	j  kmain
+
+lock_harts:
+	li   t0, 0
+	csrr t1, mhartid
+	bne  t0, t1, lock
+
+	ret
 
 lock:
 	wfi
