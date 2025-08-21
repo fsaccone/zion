d6f6d1fdc16976cde2f5035abf36c44e805ba75a
Francesco Saccone <francesco@francescosaccone.com>
2025-08-21 13:51:30 +0200

    refactor(sys/riscv): enumerate cause values

--- sys/arch/riscv/kernel/exception.c
+++ sys/arch/riscv/kernel/exception.c
@@ -2,6 +2,24 @@
 
 #include <arch/types.h>
 
+#define CAUSE_INSTR_ADDR_MISAL 0
+#define CAUSE_INSTR_ACC_FAULT  1
+#define CAUSE_ILLEGAL_INSTR    2
+#define CAUSE_BREAKPOINT       3
+#define CAUSE_LOAD_ADDR_MISAL  4
+#define CAUSE_LOAD_ACC_FAULT   5
+#define CAUSE_STORE_ADDR_MISAL 6
+#define CAUSE_STORE_ACC_FAULT  7
+#define CAUSE_UMODE_ECALL      8
+#define CAUSE_SMODE_ECALL      9
+#define CAUSE_MMODE_ECALL      11
+#define CAUSE_INSTR_PAGE_FAULT 12
+#define CAUSE_LOAD_PAGE_FAULT  13
+#define CAUSE_STORE_PAGE_FAULT 15
+#define CAUSE_DOUBLE_TRAP      16
+#define CAUSE_SOFTWARE_CHECK   18
+#define CAUSE_HARDWARE_ERROR   19
+
 void
 handle_exception(uint32_t cause)
 {
--- sys/arch/riscv/kernel/interrupt.c
+++ sys/arch/riscv/kernel/interrupt.c
@@ -2,6 +2,14 @@
 
 #include <arch/types.h>
 
+#define CAUSE_SSI 1
+#define CAUSE_MSI 3
+#define CAUSE_STI 5
+#define CAUSE_MTI 7
+#define CAUSE_SEI 9
+#define CAUSE_MEI 11
+#define CAUSE_COI 13
+
 void
 handle_interrupt(uint32_t cause)
 {
