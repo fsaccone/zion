ed5e413fd13993f84a30e0f1592aecb51410148f
Francesco Saccone <francesco@francescosaccone.com>
2025-08-21 15:37:03 +0200

    feat(sys/riscv): initialize misa

--- sys/arch/riscv/kernel/start.s
+++ sys/arch/riscv/kernel/start.s
@@ -6,12 +6,32 @@ _start:
 	la   a1, zero_ctx
 	call switch_ctx
 
+	call initialize_misa
 	call initialize_vectors
 	call lock_harts
 
 	la sp, _stack_end
 
 	j kmain
+
+initialize_misa:
+	li   t0, 0
+
+	# RV32I/64I base ISA
+	li  t1, 1 << 8
+	add t0, t0, t1
+
+	# Supervisor mode implemented
+	li  t1, 1 << 18
+	add t0, t0, t1
+
+	# User mode implemented
+	li  t1, 1 << 20
+	add t0, t0, t1
+
+	csrw misa, t0
+
+	ret
 
 initialize_vectors:
 	la   t0,    machine_trap_vector
