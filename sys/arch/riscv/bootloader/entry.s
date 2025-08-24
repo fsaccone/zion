b6059c55e8fa6776ea839b1854f6e89103b51dd2
Francesco Saccone <francesco@francescosaccone.com>
2025-08-24 15:21:23 +0200

    feat(sys/boot/riscv): initialize misa

--- sys/arch/riscv/bootloader/entry.s
+++ sys/arch/riscv/bootloader/entry.s
@@ -2,11 +2,31 @@
 .globl _entry
 
 _entry:
+	call initialize_misa
 	call zero_registers
 
 	la sp, _stack_end
 
 	j bmain
+
+initialize_misa:
+	li   t0, 0
+
+	# RV32I/64I base ISA
+	li  t1, 1 << 8
+	add t0, t0, t1
+
+	# Supervisor mode implemented
+	li  t1, 1 << 18
+	add t0, t0, t1
+
+	# User mode implemented
+	li  t1, 1 << 20
+	add t0, t0, t1
+
+	csrw misa, t0
+
+	ret
 
 zero_registers:
 	li x2,  0
