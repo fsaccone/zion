21eda2d864716819e20a386ee25895259bb83de8
Francesco Saccone <francesco@francescosaccone.com>
2025-08-24 12:45:16 +0200

    feat(sys/riscv): define int64_t, intn_t, uint64_t, uintn_t

--- sys/arch/riscv/include/arch/types.h
+++ sys/arch/riscv/include/arch/types.h
@@ -4,10 +4,22 @@
 typedef signed char  int8_t;
 typedef signed short int16_t;
 typedef signed int   int32_t;
+typedef struct {
+	int32_t low;
+	int32_t high;
+} int64_t;
+
+typedef int32_t intn_t;
 
 typedef unsigned char  uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int   uint32_t;
+typedef struct {
+	uint32_t low;
+	uint32_t high;
+} uint64_t;
+
+typedef uint32_t uintn_t;
 
 typedef float  float32_t;
 typedef double float64_t;
