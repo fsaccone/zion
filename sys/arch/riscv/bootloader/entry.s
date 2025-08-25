9d2545978a0643fbb9e3bfd680964dd252a1bae4
Francesco Saccone <francesco@francescosaccone.com>
2025-08-25 20:26:19 +0200

    refactor(sys/bootloader/riscv): use lib's clear_registers

--- sys/arch/riscv/bootloader/entry.s
+++ sys/arch/riscv/bootloader/entry.s
@@ -5,7 +5,7 @@ _entry:
 	call initialize_misa
 	call lock_harts
 	call clear_bss
-	call zero_registers
+	call clear_registers
 
 	la sp, _stack_end
 
@@ -40,37 +40,3 @@ lock_harts:
 lock:
 	wfi
 	j lock
-
-zero_registers:
-	li x2,  0
-	li x3,  0
-	li x4,  0
-	li x5,  0
-	li x6,  0
-	li x7,  0
-	li x8,  0
-	li x9,  0
-	li x10, 0
-	li x11, 0
-	li x12, 0
-	li x13, 0
-	li x14, 0
-	li x15, 0
-	li x16, 0
-	li x17, 0
-	li x18, 0
-	li x19, 0
-	li x20, 0
-	li x21, 0
-	li x22, 0
-	li x23, 0
-	li x24, 0
-	li x25, 0
-	li x26, 0
-	li x27, 0
-	li x28, 0
-	li x29, 0
-	li x30, 0
-	li x31, 0
-
-	ret
