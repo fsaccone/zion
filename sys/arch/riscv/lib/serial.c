a30a6acb31d2f619a27216984d9f2ad755ccdf06
Francesco Saccone <francesco@francescosaccone.com>
2025-08-26 11:53:53 +0200

    refactor(sys/riscv): add 0x00 to UART0 to make UART_THR

--- sys/arch/riscv/lib/serial.c
+++ sys/arch/riscv/lib/serial.c
@@ -3,7 +3,7 @@
 #include <arch/types.h>
 
 #define UART0               (uint8_t *)(0x10000000)
-#define UART_THR            (uint8_t *)(UART0)
+#define UART_THR            (uint8_t *)(UART0 + 0x00)
 #define UART_LSR            (uint8_t *)(UART0 + 0x05)
 #define UART_LSR_EMPTY_MASK (0x40)
 
