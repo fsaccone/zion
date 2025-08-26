8887b808c06cd3d5235bef6ec409bef4a03c6850
Francesco Saccone <francesco@francescosaccone.com>
2025-08-26 15:38:49 +0200

    refactor(sys): rename static function serial_print to sprint

--- sys/lib/log.c
+++ sys/lib/log.c
@@ -3,7 +3,7 @@
 #include <arch.h>
 
 static void
-serial_print(const char *str)
+sprint(const char *str)
 {
 	while (*str) {
 		while(!(*UART_LSR & UART_LSR_EMPTY_MASK));
@@ -14,16 +14,16 @@ serial_print(const char *str)
 void
 debug(const char *m)
 {
-	serial_print("DEBUG: ");
-	serial_print(m);
-	serial_print("\n");
+	sprint("DEBUG: ");
+	sprint(m);
+	sprint("\n");
 }
 
 void
 panic(const char *m)
 {
-	serial_print("PANIC: ");
-	serial_print(m);
-	serial_print("\n");
+	sprint("PANIC: ");
+	sprint(m);
+	sprint("\n");
 	for (;;);
 }
