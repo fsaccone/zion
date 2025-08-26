5f40940b6c7da454d946beaaa5cc82e3b31c4a05
Francesco Saccone <francesco@francescosaccone.com>
2025-08-26 17:10:27 +0200

    feat(sys): make ptables const pointers to uintn_t

--- sys/include/ptable.h
+++ sys/include/ptable.h
@@ -1,9 +1,11 @@
 #ifndef _PTABLE_H
 #define _PTABLE_H
 
-const void **p1table;
-const void **p2table;
-const void **p3table;
-const void **p4table;
+#include <arch.h>
+
+uintn_t *const p1table;
+uintn_t *const p2table;
+uintn_t *const p3table;
+uintn_t *const p4table;
 
 #endif
