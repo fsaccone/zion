8e0cd10bc6eeebec86b92340b292c2af895e873a
Francesco Saccone <francesco@francescosaccone.com>
2025-08-26 16:09:53 +0200

    feat(sys): define p1,2,3,4table pointers in ptable.h

---
+++ sys/arch/riscv/lib/ptable.s
@@ -0,0 +1,11 @@
+.section .rodata
+.globl p1table
+.globl p2table
+.globl p3table
+.globl p4table
+.align 12
+
+p1table: .space 4096
+p2table: .space 4096
+p3table: .space 4096
+p4table: .space 4096
---
+++ sys/include/ptable.h
@@ -0,0 +1,9 @@
+#ifndef _PTABLE_H
+#define _PTABLE_H
+
+const void **p1table;
+const void **p2table;
+const void **p3table;
+const void **p4table;
+
+#endif
