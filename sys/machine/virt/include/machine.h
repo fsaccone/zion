36bb594a709233c24f68ee8584e5b302fc725723
Francesco Saccone <francesco@francescosaccone.com>
2025-08-29 13:11:32 +0200

    fix(sys): use uintn_t instead of void * when operating pointers

--- sys/machine/virt/include/machine.h
+++ sys/machine/virt/include/machine.h
@@ -1,18 +1,17 @@
 #ifndef _MACHINE_H
 #define _MACHINE_H
 
-#define SUB_PTRS(p, q) (void *)((long)(p) - (long)(q))
-#define SUM_PTRS(p, q) (void *)((long)(p) + (long)(q))
+#include <arch.h>
 
 #define RAM_SIZE (128 * 1024 * 1024)
 
-#define KERNEL_START (void *)0x80000000
-#define KERNEL_SIZE  (void *)0xa0000
+#define KERNEL_START (uintn_t)0x80000000
+#define KERNEL_SIZE  (uintn_t)0xa0000
 
 #define FREE_MEMORY_REGIONS 1
-void *freememregions[FREE_MEMORY_REGIONS][2] = {
-	/* start                                size */
-	{  SUM_PTRS(KERNEL_START, KERNEL_SIZE), SUB_PTRS(RAM_SIZE, SUM_PTRS(KERNEL_START, KERNEL_SIZE)) },
+uintn_t freememregions[FREE_MEMORY_REGIONS][2] = {
+	/* start                       size */
+	{  KERNEL_START + KERNEL_SIZE, RAM_SIZE - (KERNEL_START + KERNEL_SIZE) },
 };
 
 #define HAS_UART
