b2af5074cecdd56d552dd3c94947839eed15b976
Francesco Saccone <francesco@francescosaccone.com>
2025-08-29 12:27:46 +0200

    refactor(sys): define HAS_UART if machine defines UART macros

--- sys/machine/virt/include/machine.h
+++ sys/machine/virt/include/machine.h
@@ -3,6 +3,8 @@
 
 #define RAM_SIZE 128 * 1024 * 1024
 
+#define HAS_UART
+
 #define UART0               (uint8_t *)(0x10000000)
 #define UART_THR            (uint8_t *)(UART0 + 0x00)
 #define UART_LSR            (uint8_t *)(UART0 + 0x05)
