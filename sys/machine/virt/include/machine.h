8e4d275fb272b3b9704a272804cc13632524e4f7
Francesco Saccone <francesco@francescosaccone.com>
2025-08-29 13:05:49 +0200

    refactor(sys): cast machine KERNEL_* macros to pointers

--- sys/machine/virt/include/machine.h
+++ sys/machine/virt/include/machine.h
@@ -1,15 +1,18 @@
 #ifndef _MACHINE_H
 #define _MACHINE_H
 
+#define SUB_PTRS(p, q) (void *)((long)(p) - (long)(q))
+#define SUM_PTRS(p, q) (void *)((long)(p) + (long)(q))
+
 #define RAM_SIZE (128 * 1024 * 1024)
 
-#define KERNEL_START 0x80000000
-#define KERNEL_SIZE  0xa0000
+#define KERNEL_START (void *)0x80000000
+#define KERNEL_SIZE  (void *)0xa0000
 
 #define FREE_MEMORY_REGIONS 1
 void *freememregions[FREE_MEMORY_REGIONS][2] = {
-	/* start                       size */
-	{  KERNEL_START + KERNEL_SIZE, RAM_SIZE - KERNEL_START - KERNEL_SIZE },
+	/* start                                size */
+	{  SUM_PTRS(KERNEL_START, KERNEL_SIZE), SUB_PTRS(RAM_SIZE, SUM_PTRS(KERNEL_START, KERNEL_SIZE)) },
 };
 
 #define HAS_UART
