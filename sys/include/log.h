d7c29b2e7650890de17f283243a280bc18870c73
Francesco Saccone <francesco@francescosaccone.com>
2025-08-30 16:18:09 +0200

    chore(sys): add comments to log.h

--- sys/include/log.h
+++ sys/include/log.h
@@ -1,9 +1,16 @@
 #ifndef _LOG_H
 #define _LOG_H
 
+/* Prints a message indented to the last debughead */
 void debug(char *m);
+
+/* Prints a message prefixed with 'DEBUG: ' */
 void debughead(char *h);
+
+/* Prints an address indented to the last debughead */
 void debugptr(void *ptr);
+
+/* Prints an error message and panics */
 void panic(char *m);
 
 #endif
