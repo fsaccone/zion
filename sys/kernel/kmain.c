9382d4427df04b5673a05adbb60a17d4c1204b6d
Francesco Saccone <francesco@francescosaccone.com>
2025-08-30 15:32:06 +0200

    feat(sys): free all machine's free memory regions

--- sys/kernel/kmain.c
+++ sys/kernel/kmain.c
@@ -1,9 +1,28 @@
+#include <arch.h>
 #include <log.h>
+#include <machine.h>
+#include <pmem.h>
 
 void
 kmain(void)
 {
+	int i, *p, *q;
+	uintn_t freemem[FREE_MEMORY_REGIONS_LEN][2] = { FREE_MEMORY_REGIONS };
+
 	debug("Kernel start.");
+
+	for (i = 0; i < FREE_MEMORY_REGIONS_LEN; i++) {
+		uintn_t start = freemem[i][0],
+		        size  = freemem[i][1];
+
+		debug("Freeing user memory:");
+		debug("  From:");
+		debugptr((void *)start);
+		debug("  To:");
+		debugptr((void *)(start + size));
+
+		pfreerange((void *)start, (void *)(start + size));
+	}
 
 	for (;;);
 }
