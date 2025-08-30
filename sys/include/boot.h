b5fdbbe62c12f4c73b50d12ba3239e21ce557f0a
Francesco Saccone <francesco@francescosaccone.com>
2025-08-30 16:19:11 +0200

    chore(sys): add comments to boot.h

--- sys/include/boot.h
+++ sys/include/boot.h
@@ -1,7 +1,11 @@
+/* ARCHITECTURE-SPECIFIC */
 #ifndef _BOOT_H
 #define _BOOT_H
 
+/* Clears the bss section of the kernel code */
 void clearbss(void);
+
+/* Sets all general-purpose registers to 0 */
 void clearregs(void);
 
 #endif
