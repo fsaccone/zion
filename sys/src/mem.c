20b01b90901cfd521ad1cdebac182cb913e00b60
Francesco Saccone <francesco@francescosaccone.com>
2025-08-30 16:42:18 +0200

    refactor(sys): modularize by defining freeallmem in private mem.h

---
+++ sys/src/mem.c
@@ -0,0 +1,24 @@
+#include <arch.h>
+#include <log.h>
+#include <machine.h>
+#include <pmem.h>
+
+void
+freeallmem(void)
+{
+	int i;
+	uintn_t freemem[FREE_MEMORY_REGIONS_LEN][2] = { FREE_MEMORY_REGIONS };
+
+	for (i = 0; i < FREE_MEMORY_REGIONS_LEN; i++) {
+		uintn_t start = freemem[i][0],
+		        size  = freemem[i][1];
+
+		debughead("Freeing user memory:");
+		debug("From:");
+		debugptr((void *)start);
+		debug("To:");
+		debugptr((void *)(start + size));
+
+		pfreerange((void *)start, (void *)(start + size));
+	}
+}
