9ab6ef21690d419bab082d053c1421f3df49c1bb
Francesco Saccone <francesco@francescosaccone.com>
2025-08-31 20:24:43 +0200

    feat(sys): define createpagetable in vmem.h

---
+++ sys/arch/riscv64/lib/vmem.c
@@ -0,0 +1,15 @@
+#include <vmem.h>
+
+#include <arch.h>
+#include <pmem.h>
+
+#define PAGE_TABLE_ENTRIES 1024
+
+void *
+createpagetable(void)
+{
+	/* Since palloc allocates frames aligned with PAGE_SIZE=4096, it is
+	   assured that the page table is also aligned to 4096. Also, palloc
+	   fills all allocated frames with zeros, and that is what we want. */
+	return palloc(PAGE_TABLE_ENTRIES * sizeof(uintn));
+}
---
+++ sys/include/vmem.h
@@ -0,0 +1,9 @@
+/* ARCHITECTURE-SPECIFIC */
+#ifndef _VMEM_H
+#define _VMEM_H
+
+/* Creates an empty page table full of invalid entries and returns its
+   pointer */
+void *createpagetable(void);
+
+#endif
