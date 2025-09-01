2843fe872956381870cbeb3ac8f7eefa1cb1cff8
Francesco Saccone <francesco@francescosaccone.com>
2025-09-01 13:34:47 +0200

    feat(sys/riscv): define private levelpagetable function

--- sys/arch/riscv64/lib/vmem.c
+++ sys/arch/riscv64/lib/vmem.c
@@ -1,11 +1,81 @@
 #include <vmem.h>
 
 #include <arch.h>
+#include <log.h>
 #include <pmem.h>
 
 /* The used scheme is Sv57 */
 
 #define PAGE_TABLE_ENTRIES 512
+#define PAGE_TABLE_LEVELS  5
+
+/* Returns the first valid l-level page table it encounters, or NULL if pt is
+   full. It creates one if needed */
+static void *
+levelpagetable(void *pt, int l)
+{
+	int i, lvlidx[PAGE_TABLE_LEVELS] = { 0 };
+	uint64 *lastpt;
+
+	if (l < 1 && l > PAGE_TABLE_LEVELS)
+		panic("lastlevelpt: Passed non-existent level");
+
+	lastpt = (uint64 *)pt;
+	for (i = 0; i < l - 1; i++) {
+		/* We need to keep track of the last entry index of each level
+		   in case we need to go backwards after walking through a
+		   full table: lvlidx does that */
+		for (; lvlidx[i] < PAGE_TABLE_ENTRIES; lvlidx[i]++) {
+			uint64 *pte = (void *)((uintn)lastpt
+			                       + lvlidx[i] * sizeof(uint64));
+
+			/* If V is 0, create and walk in a new pt */
+			if (!(*pte & 1)) {
+				lastpt = createpagetable();
+
+				/* Set PPN to point to new table (since lastpt
+				   is aligned, upper reserved bits and option
+				   bits are already 0) */
+				*pte = (uintn)lastpt;
+
+				/* Set V to 1 */
+				*pte |= 1;
+
+				goto nextlevel;
+			}
+
+			/* If R, W and X are all 0, walk in the pt at PPN */
+			if (!(*pte & 0b1110)) {
+				uint64 addr = *pte;
+
+				/* Set all option bits to 0 */
+				addr >>= 10;
+				addr <<= 10;
+
+				/* Set upper reserved bits to 0 */
+				addr <<= 10;
+				addr >>= 10;
+
+				lastpt = (uint64 *)addr;
+
+				goto nextlevel;
+			}
+		}
+
+		/* If we walked through the whole root page table, then it is
+		   full */
+		if (lvlidx[0] == PAGE_TABLE_ENTRIES - 1)
+			return NULL;
+
+		/* If none of the entries are walkable, there is no space in
+		   lastpt: we need to go back one level and continue from
+		   where we left */
+		i -= 2;
+nextlevel:
+	}
+
+	return lastpt;
+}
 
 void *
 createpagetable(void)
