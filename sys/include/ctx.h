e82b90a92891f408bc3fecd5d57848156edd0c84
Francesco Saccone <francesco@francescosaccone.com>
2025-09-03 18:49:01 +0200

    feat(sys): define switchctx in ctx.h

---
+++ sys/include/ctx.h
@@ -0,0 +1,9 @@
+/* ARCHITECTURE-SPECIFIC */
+#ifndef _CTX_H
+#define _CTX_H
+
+/* Make CPU switch to the context in new after saving its current context in
+   old */
+void switchctx(void *old, void *new);
+
+#endif
