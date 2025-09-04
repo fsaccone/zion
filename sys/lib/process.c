027751ae649dab242e3365d84b8aeb282d203ebf
Francesco Saccone <francesco@francescosaccone.com>
2025-09-04 12:42:17 +0200

    feat(sys): define pidbitmap and unusedpid function

--- sys/lib/process.c
+++ sys/lib/process.c
@@ -6,11 +6,35 @@
 static struct process init     = { 0 };
 static int            initdone = 0;
 
+static u8             pidbitmap[PID_MAX / 8] = { 0 };
+
+/* Returns the first unused PID from pidbitmap and sets it to used. Returns 0
+   if pidbitmap is full */
+static u16 unusedpid(void);
+
+u16
+unusedpid(void)
+{
+	u16 i;
+
+	for (i = 0; i < PID_MAX; i++) {
+		if (!(pidbitmap[i / 8] & (1 << (i % 8)))) {
+			pidbitmap[i / 8] |= (1 << (i % 8));
+			return i;
+		}
+	}
+
+	return 0;
+}
+
 struct process *
 initprocess(void)
 {
 	if (initdone)
 		panic("initprocess: Called for the second time.");
+
+	/* Set bit 0 to used */
+	pidbitmap[0] |= 1;
 
 	init.pid = 0;
 	init.state = CREATED;
