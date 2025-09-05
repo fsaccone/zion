1f54ef98e7d0b94e0b039d008e989e24c8e058ac
Francesco Saccone <francesco@francescosaccone.com>
2025-09-05 18:32:00 +0200

    chore(sys): add comments to config.mk

--- config.mk
+++ config.mk
@@ -1,6 +1,10 @@
+# Target
+
 # To build /sys/, ARCH should match the architecture of MACHINE
 MACHINE = virt
 ARCH    = riscv64
+
+# Toolchain
 
 CROSS_COMPILE = $(ARCH)-linux-musl-
 
@@ -11,9 +15,13 @@ LD      = $(CROSS_COMPILE)ld
 OBJCOPY = $(CROSS_COMPILE)objcopy
 STRIP   = $(CROSS_COMPILE)strip
 
+# Toolchain flags
+
 ASFLAGS = $(EXTRAASFLAGS)
 CFLAGS  = $(EXTRACFLAGS) -std=c90 -ffreestanding -Wall -Wextra
 LDFLAGS = $(EXTRALDFLAGS)
+
+# Qemu (for testing purposes only)
 
 QEMU        = qemu-system-$(ARCH)
 QEMUCPUS    = 4
