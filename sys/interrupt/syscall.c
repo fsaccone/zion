534bc9107ecb60929b09d6d75e223b60224b27b9
Francesco Saccone <francesco@francescosaccone.com>
2025-09-08 20:08:51 +0200

    feat(sys): call pcleanup from shutdown and reboot syscalls

--- sys/interrupt/syscall.c
+++ sys/interrupt/syscall.c
@@ -2,6 +2,7 @@
 
 #include <log.h>
 #include <machine/sys.h>
+#include <pmem.h>
 
 int
 syscall(u16 type, un args[SYSCALL_ARGS])
@@ -10,9 +11,11 @@ syscall(u16 type, un args[SYSCALL_ARGS])
 
 	switch (type) {
 	case SYSCALL_SHUTDOWN:
+		pcleanup();
 		DO_SYS_SHUTDOWN;
 		break;
 	case SYSCALL_REBOOT:
+		pcleanup();
 		DO_SYS_REBOOT;
 		break;
 	default:
