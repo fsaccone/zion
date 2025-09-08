35b53bcff6e149598cfce4522fac2e6c9b6989a4
Francesco Saccone <francesco@francescosaccone.com>
2025-09-08 17:07:00 +0200

    feat(sys): set interrupt panic message to interrupt type name

--- sys/interrupt/interrupt.c
+++ sys/interrupt/interrupt.c
@@ -1,8 +1,29 @@
+#include <interrupt.h>
 #include <log.h>
 
 void
 interrupt(void)
 {
-	setpanicmsg("Unimplemented");
+	switch (getinterrupttype()) {
+	case INTERRUPT_TYPE_EXCEPTION:
+		setpanicmsg("Exception.");
+		break;
+	case INTERRUPT_TYPE_HARDWARE:
+		setpanicmsg("Hardware.");
+		break;
+	case INTERRUPT_TYPE_SOFTWARE:
+		setpanicmsg("Software.");
+		break;
+	case INTERRUPT_TYPE_TIMER:
+		setpanicmsg("Timer.");
+		break;
+	case INTERRUPT_TYPE_SYSCALL:
+		setpanicmsg("Syscall.");
+		break;
+	case INTERRUPT_TYPE_UNKNOWN:
+	default:
+		setpanicmsg("Unknown.");
+	}
+
 	panic("interrupt");
 }
