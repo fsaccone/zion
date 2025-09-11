a3fb01cbd99c5f9e93b7e9efdfb910609c9ce73a
Francesco Saccone <francesco@francescosaccone.com>
2025-09-11 18:42:38 +0200

    feat(sys): start all cores after core 0 stops initializing

--- sys/kernel/kernel.c
+++ sys/kernel/kernel.c
@@ -1,17 +1,32 @@
+#include <arch/types.h>
 #include <config.h>
 #include <core.h>
 #include <driver.h>
 #include <interrupt.h>
 #include <log.h>
+#include <spinlock.h>
 
 #include "drivers.h"
 #include "mem.h"
 
+static u8 initdone = 0;
+static struct lock l = { 0 };
+
 void
 kernel(void)
 {
+	u16 c = core();
+
 	/* For every core except 0 (main core). */
-	if (core()) {
+	if (c) {
+		while (!initdone);
+
+		acquirelock(&l);
+		debug("Core ");
+		debugintbase10u(c);
+		debug(" started.\n");
+		releaselock(&l);
+
 		for (;;);
 	}
 
@@ -28,6 +43,7 @@ kernel(void)
 	if (freeallmem())
 		panic("kernel");
 
-	setpanicmsg("Reached end of kernel execution.");
-	panic("kernel");
+	initdone = 1;
+
+	for (;;);
 }
