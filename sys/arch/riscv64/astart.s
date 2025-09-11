2a5d52293382ffdda56b507fcd8806aa3ebb9367
Francesco Saccone <francesco@francescosaccone.com>
2025-09-11 16:09:12 +0200

    feat(sys/riscv): define and call inittimer

--- sys/arch/riscv64/astart.s
+++ sys/arch/riscv64/astart.s
@@ -11,6 +11,8 @@ astart:
 	call initmstatus
 	call initsstatus
 
+	call inittimer
+
 	la   t0,    trapvec
 	csrw stvec, t0
 
@@ -89,6 +91,33 @@ initsstatus:
 	or t0, t0, t1
 
 	csrw sstatus, t0
+
+	ret
+
+inittimer:
+	# Set mie.STIE to 1
+	csrr t0,  sie
+	li   t1,  (1 << 5)
+	or   t0,  t0, t1
+	csrw sie, t0
+
+	# Enable the sstc extension
+	csrr t0,      menvcfg
+	li   t1,      (1 << 63)
+	or   t0,      t0, t1
+	csrw menvcfg, t0
+
+	# Allow supervisor to use stimecmp and time
+	csrr t0,         mcounteren
+	li   t1,         (1 << 1)
+	or   t0,         t0, t1
+	csrw mcounteren, t0
+
+	# First timer interrupt
+	csrr t0,       time
+	li   t1,       1000000
+	add  t0,       t0, t1
+	csrw stimecmp, t0
 
 	ret
 
