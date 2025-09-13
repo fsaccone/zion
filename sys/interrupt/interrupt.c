4d882765e4c673047ffcd6b703505615ed88011e
Francesco Saccone <francesco@francescosaccone.com>
2025-09-13 16:58:00 +0200

    fix(sys): acquire spinlock before any log operation in interrupt

--- sys/interrupt/interrupt.c
+++ sys/interrupt/interrupt.c
@@ -1,8 +1,11 @@
 #include <interrupt.h>
 #include <log.h>
+#include <spinlock.h>
 #include <timer.h>
 
 #include "syscall.h"
+
+static struct lock l = { 0 };
 
 void
 interrupt(void)
@@ -11,24 +14,36 @@ interrupt(void)
 
 	switch (interrupttype()) {
 	case INTERRUPT_TYPE_SYSCALL:
+		acquirelock(&l);
 		if (syscall((u16)args[0], &args[1]))
 			panic("interrupt");
+		releaselock(&l);
 		return;
 	case INTERRUPT_TYPE_EXCEPTION:
+		acquirelock(&l);
 		setpanicmsg("Exception.");
 		panic("interrupt");
+		releaselock(&l);
 		break;
 	case INTERRUPT_TYPE_HARDWARE:
+		acquirelock(&l);
 		debug("interrupt: Hardware.\n");
+		releaselock(&l);
 		break;
 	case INTERRUPT_TYPE_SOFTWARE:
+		acquirelock(&l);
 		debug("interrupt: Software.\n");
+		releaselock(&l);
 		break;
 	case INTERRUPT_TYPE_TIMER:
 		setupnexttimer();
+		acquirelock(&l);
 		debug("interrupt: Timer.\n");
+		releaselock(&l);
 		break;
 	default:
+		acquirelock(&l);
 		debug("interrupt: Unknown.\n");
+		releaselock(&l);
 	}
 }
