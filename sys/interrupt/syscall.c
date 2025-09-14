edb3ba792a3868225d9eb876340e6e30091d5dcc
Francesco Saccone <francesco@francescosaccone.com>
2025-09-14 15:03:23 +0200

    fix(sys): acquire spinlock before shutting down or rebooting

--- sys/interrupt/syscall.c
+++ sys/interrupt/syscall.c
@@ -3,6 +3,9 @@
 #include <machine/sys.h>
 #include <panic.h>
 #include <pmem.h>
+#include <spinlock.h>
+
+static struct lock l = { 0 };
 
 int
 syscall(u16 type, un args[SYSCALL_ARGS])
@@ -11,12 +14,22 @@ syscall(u16 type, un args[SYSCALL_ARGS])
 
 	switch (type) {
 	case SYSCALL_SHUTDOWN:
+		/* This lock will never be released. */
+		acquirelock(&l);
+
 		pcleanup();
+
 		DO_SYS_SHUTDOWN;
+
 		break;
 	case SYSCALL_REBOOT:
+		/* This lock will never be released. */
+		acquirelock(&l);
+
 		pcleanup();
+
 		DO_SYS_REBOOT;
+
 		break;
 	default:
 		setpanicmsg("Non-existent system call.");
