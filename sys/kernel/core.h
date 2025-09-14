8c47ce95d4d74d886f9036cce69169a5a660f040
Francesco Saccone <francesco@francescosaccone.com>
2025-09-14 17:54:55 +0200

    chore(sys): comment that coremain is to be called after sys init

--- sys/kernel/core.h
+++ sys/kernel/core.h
@@ -3,7 +3,8 @@
 
 #include <arch/types.h>
 
-/* The main function of every CPU core with core ID c. */
+/* The main function of every CPU core with core ID c to be called after system
+   has been initialized. */
 void coremain(u16 c);
 
 #endif
