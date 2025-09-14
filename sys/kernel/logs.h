448878b88cdddcc213e7e1c4168b42929a34e30f
Francesco Saccone <francesco@francescosaccone.com>
2025-09-14 17:16:29 +0200

    feat(sys): remove 'loaded' from kernel/logs.h log prefixes

--- sys/kernel/logs.h
+++ sys/kernel/logs.h
@@ -1,7 +1,7 @@
 #ifndef __LOGS_H
 #define __LOGS_H
 
-#define CPU_LOG_PRE "CPU loaded     : "
-#define MEM_LOG_PRE "Memory loaded  : "
+#define CPU_LOG_PRE "CPU    : "
+#define MEM_LOG_PRE "Memory : "
 
 #endif
