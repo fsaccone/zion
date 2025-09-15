f9c0e4981cfbd711c5036d1aa6a200e1383a3362
Francesco Saccone <francesco@francescosaccone.com>
2025-09-15 21:55:02 +0200

    chore: reorder, clean and remove some comments in config.mk

--- config.mk
+++ config.mk
@@ -1,21 +1,16 @@
 # Target.
-
-# To build /sys/, ARCH should match the architecture of MACHINE.
-MACHINE = virt
 ARCH    = riscv64
+MACHINE = virt
 
 # Toolchain.
-
 CROSS_COMPILE = $(ARCH)-linux-musl-
-
-AR      = $(CROSS_COMPILE)ar
-AS      = $(CROSS_COMPILE)as
-CC      = $(CROSS_COMPILE)cc
-LD      = $(CROSS_COMPILE)ld
-OBJCOPY = $(CROSS_COMPILE)objcopy
+AR            = $(CROSS_COMPILE)ar
+AS            = $(CROSS_COMPILE)as
+CC            = $(CROSS_COMPILE)cc
+LD            = $(CROSS_COMPILE)ld
+OBJCOPY       = $(CROSS_COMPILE)objcopy
 
 # Toolchain flags.
-
 ASFLAGS = $(EXTRAASFLAGS)
 CFLAGS  = $(EXTRACFLAGS) -std=c90 -ffreestanding -Wall -Wextra
 LDFLAGS = $(EXTRALDFLAGS) -s
