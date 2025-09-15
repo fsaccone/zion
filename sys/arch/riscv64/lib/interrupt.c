a8ffd019b29162386b51837b9accfc1da97d850b
Francesco Saccone <francesco@francescosaccone.com>
2025-09-15 17:50:52 +0200

    refactor(sys/riscv): define PLIC_MEM_CLAIM macro to simplify code

--- sys/arch/riscv64/lib/interrupt.c
+++ sys/arch/riscv64/lib/interrupt.c
@@ -3,13 +3,15 @@
 #include <interrupt.h>
 #include <machine/mem.h>
 
+#define PLIC_MEM_CLAIM(c) ((uptr)PLIC + 0x2004 + (c) * 0x2000)
+
 u8
 interruptdriver(struct driver *drv)
 {
 	if (interrupttype() != INTERRUPT_TYPE_HARDWARE)
 		return -1;
 
-	switch (*(u32 *)((uptr)PLIC + 0x2004 + core() * 0x2000)) {
+	switch (*(u32 *)PLIC_MEM_CLAIM(core())) {
 	case UART0_IRQ:
 		*drv = driver_uart();
 		return 0;
