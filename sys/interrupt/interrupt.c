a096aeaed46a1ff0ea85482676012886392cd898
Francesco Saccone <francesco@francescosaccone.com>
2025-09-15 16:00:15 +0200

    feat(sys): move all kernel interrupt cases to both kernel and user

--- sys/interrupt/interrupt.c
+++ sys/interrupt/interrupt.c
@@ -1,5 +1,8 @@
+#include <console.h>
+#include <driver.h>
 #include <interrupt.h>
 #include <panic.h>
+#include <timer.h>
 
 #include "kernel.h"
 #include "syscall.h"
@@ -11,20 +14,40 @@ interrupt(void)
 	ureg *args   = interruptargs();
 	u8    isuser = interruptisuser(),
 	      type   = interrupttype();
+	struct driver drv;
 
 	switch (type) {
 	case INTERRUPT_TYPE_SYSCALL:
 		if (syscall((u16)args[0], &args[1])) {
-			tracepanicmsg("userinterrupt");
+			tracepanicmsg("interrupt");
 			panic();
 		}
 
 		break;
-	default:
-	}
+	case INTERRUPT_TYPE_EXCEPTION:
+		setpanicmsg("Exception.");
+		tracepanicmsg("interrupt");
+		panic();
 
-	if (isuser)
-		userinterrupt(args);
-	else
-		kernelinterrupt();
+		break;
+	case INTERRUPT_TYPE_HARDWARE:
+		if (interruptdriver(&drv)) {
+			setpanicmsg("Unknown hardware interrupt.");
+			tracepanicmsg("interrupt");
+			panic();
+		}
+
+		if (drv.interrupt)
+			drv.interrupt();
+
+		break;
+	case INTERRUPT_TYPE_SOFTWARE:
+		(void)consolewrite("interrupt: Software.\n");
+		break;
+	case INTERRUPT_TYPE_TIMER:
+		setupnexttimer();
+		break;
+	default:
+		(void)consolewrite("interrupt: Unknown.\n");
+	}
 }
