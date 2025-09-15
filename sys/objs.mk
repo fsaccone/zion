dcb6298ff80ca4e1b56d2ed046877c6f27460a16
Francesco Saccone <francesco@francescosaccone.com>
2025-09-15 17:16:49 +0200

    build(sys): group all interrupt, kernel and lib objs together

--- sys/objs.mk
+++ sys/objs.mk
@@ -6,11 +6,10 @@ include ../config.mk
 OBJS_START = arch/$(ARCH)/machine/$(MACHINE)/start.o
 
 # Object files from interrupt/.
-OBJS_INTERRUPT_SYSCALL = interrupt/syscall/shutdown.o \
-                         interrupt/syscall/reboot.o
-OBJS_INTERRUPT         = $(OBJS_INTERRUPT_SYSCALL) \
-                         interrupt/interrupt.o \
-                         interrupt/syscall.o
+OBJS_INTERRUPT = interrupt/interrupt.o \
+                 interrupt/syscall.o \
+                 interrupt/syscall/reboot.o \
+                 interrupt/syscall/shutdown.o
 
 # Object files from kernel/ with the addition of arch/ARCH/astart.o.
 OBJS_KERNEL = arch/$(ARCH)/astart.o \
@@ -20,22 +19,20 @@ OBJS_KERNEL = arch/$(ARCH)/astart.o \
               kernel/mem.o
 
 # Object files from arch/ARCH/lib/ and lib/.
-OBJS_ARCH_LIB   = arch/$(ARCH)/lib/atomic.o \
-                  arch/$(ARCH)/lib/core.o \
-                  arch/$(ARCH)/lib/ctx.o \
-                  arch/$(ARCH)/lib/interrupt.o \
-                  arch/$(ARCH)/lib/timer.o \
-                  arch/$(ARCH)/lib/user.o \
-                  arch/$(ARCH)/lib/vmem.o
-OBJS_LIB_DRIVER = lib/driver/uart.o
-OBJS_LIB        = $(OBJS_ARCH_LIB) \
-                  $(OBJS_LIB_DRIVER) \
-                  lib/console.o \
-                  lib/pagetable.o \
-                  lib/panic.o \
-                  lib/pmem.o \
-                  lib/process.o \
-                  lib/spinlock.o
+OBJS_LIB = arch/$(ARCH)/lib/atomic.o \
+           arch/$(ARCH)/lib/core.o \
+           arch/$(ARCH)/lib/ctx.o \
+           arch/$(ARCH)/lib/interrupt.o \
+           arch/$(ARCH)/lib/timer.o \
+           arch/$(ARCH)/lib/user.o \
+           arch/$(ARCH)/lib/vmem.o \
+           lib/console.o \
+           lib/driver/uart.o \
+           lib/pagetable.o \
+           lib/panic.o \
+           lib/pmem.o \
+           lib/process.o \
+           lib/spinlock.o
 
 # All object files.
 OBJS = $(OBJS_START) $(OBJS_INTERRUPT) $(OBJS_KERNEL) $(OBJS_LIB)
