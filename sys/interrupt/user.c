526aa30fc105b4c0c6182c69e6508f05f69e2555
Francesco Saccone <francesco@francescosaccone.com>
2025-09-15 15:57:53 +0200

    feat(sys): enable syscall interrupts for kernel too

--- sys/interrupt/user.c
+++ sys/interrupt/user.c
@@ -2,21 +2,9 @@
 
 #include <arch/types.h>
 #include <interrupt.h>
-#include <panic.h>
-
-#include "syscall.h"
 
 void
 userinterrupt(ureg args[INTERRUPT_ARGS])
 {
-	switch (interrupttype()) {
-	case INTERRUPT_TYPE_SYSCALL:
-		if (syscall((u16)args[0], &args[1])) {
-			tracepanicmsg("userinterrupt");
-			panic();
-		}
-
-		break;
-	default:
-	}
+	(void)args;
 }
