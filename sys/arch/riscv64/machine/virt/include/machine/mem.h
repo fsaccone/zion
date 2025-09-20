9b2367a828718a2b5aea4a6b55f6652866585053
Francesco Saccone <francesco@francescosaccone.com>
2025-09-20 17:19:39 +0200

    refactor(sys): define BINARY_SIZE, now KERNEL_SIZE, in machine/mem.h

--- sys/arch/riscv64/machine/virt/include/machine/mem.h
+++ sys/arch/riscv64/machine/virt/include/machine/mem.h
@@ -2,16 +2,17 @@
 #define _MACHINE_MEM_H
 
 #include <arch/types.h>
-#include <binary.h>
 
 #define RAM_SIZE (4L * 1024L * 1024L * 1024L)
 
 #define KERNEL_START 0x80000000
+/* (256 K) */
+#define KERNEL_SIZE (256 * 1024)
 
 #define FREE_MEMORY_REGIONS_LEN 1
 #define FREE_MEMORY_REGIONS \
 	/* start                       end     */ \
-	{  KERNEL_START + BINARY_SIZE, RAM_SIZE },
+	{  KERNEL_START + KERNEL_SIZE, RAM_SIZE },
 
 #define PLIC 0xc000000
 
--- sys/include/binary.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BINARY_H
-#define _BINARY_H
-
-/* The size of the kernel binary. */
-/* (256 K) */
-#define BINARY_SIZE (256 * 1024)
-
-#endif
