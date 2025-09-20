b40c6c7852a7ffc871247e43e7102110fad4a235
Francesco Saccone <francesco@francescosaccone.com>
2025-09-20 16:58:44 +0200

    build(sys): define and user SBIN_ENABLED_PROGRAMS

--- config.mk
+++ config.mk
@@ -18,6 +18,9 @@ ASFLAGS = $(EXTRAASFLAGS)
 CFLAGS  = $(EXTRACFLAGS) -std=c90 -ffreestanding -Wall -Wextra
 LDFLAGS = $(EXTRALDFLAGS) -s
 
+# /sbin/
+SBIN_ENABLED_PROGRAMS = init
+
 # /sys/
 # 256 K = 256 * 1024 = 262144
 SYS_BINARY_SIZE = 262144
