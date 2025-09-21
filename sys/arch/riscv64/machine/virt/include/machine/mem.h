80aa4d6212ba85836ebd44b088a5317d4b4f1b5b
Francesco Saccone <francesco@francescosaccone.com>
2025-09-21 16:55:59 +0200

    fix(sys): cast all addresses and sizes to uptr

--- sys/arch/riscv64/machine/virt/include/machine/mem.h
+++ sys/arch/riscv64/machine/virt/include/machine/mem.h
@@ -4,20 +4,21 @@
 #include <arch/types.h>
 #include <config.h>
 
-#define RAM_SIZE (4L * 1024L * 1024L * 1024L)
+/* (4G = 4 * 1024 * 1024 * 1024 = 4294967296) */
+#define RAM_SIZE (uptr)4294967296
 
-#define KERNEL_START 0x80000000
-#define KERNEL_SIZE BINARY_SIZE
+#define KERNEL_START (uptr)0x80000000
+#define KERNEL_SIZE  (uptr)BINARY_SIZE
 
 #define FREE_MEMORY_REGIONS_LEN 1
 #define FREE_MEMORY_REGIONS \
 	/* start                       end     */ \
 	{  KERNEL_START + KERNEL_SIZE, RAM_SIZE },
 
-#define PLIC 0xc000000
+#define PLIC (uptr)0xc000000
 
 /* Device base addresses. */
-#define UART0 0x10000000
+#define UART0 (uptr)0x10000000
 
 /* Device IRQs. */
 #define UART0_IRQ 10
