a508f20a3b8e112c54882ca25a89a83c9c33f999
Francesco Saccone <francesco@francescosaccone.com>
2025-09-21 19:46:45 +0200

    build: remove SBIN_ENABLED_PROGRAMS definition

--- config.mk
+++ config.mk
@@ -15,9 +15,6 @@ ASFLAGS = $(EXTRAASFLAGS)
 CFLAGS  = $(EXTRACFLAGS) -std=c90 -ffreestanding -Wall -Wextra
 LDFLAGS = $(EXTRALDFLAGS) -s
 
-# /sbin/
-SBIN_ENABLED_PROGRAMS = init
-
 # /sys/
 # 256 K = 256 * 1024 = 262144
 SYS_BINARY_SIZE = 262144
