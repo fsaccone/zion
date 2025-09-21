7618d4f199f07f18c49c7cbd8a0c9a2ead99f639
Francesco Saccone <francesco@francescosaccone.com>
2025-09-21 19:21:05 +0200

    feat(sys): define strncmp in string.h

---
+++ sys/lib/string.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+#include <arch/types.h>
+
+u8
+strncmp(char *s1, char *s2, uptr n)
+{
+	for (; n > 0; n--, s1++, s2++) {
+		if (*s1 != *s2)
+			return *(u8 *)s1 - *(u8 *)s2;
+
+		if (!*s1)
+			return 0;
+	}
+
+	return 0;
+}
