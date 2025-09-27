968ca393c02b9cda4e52974c711f5f0246e1dce1
Francesco Saccone <francesco@francescosaccone.com>
2025-09-27 14:05:34 +0200

    feat(sys): define structs elffileflags and elfprogflags

--- sys/lib/elf.c
+++ sys/lib/elf.c
@@ -4,41 +4,41 @@
 #include <arch/types.h>
 
 struct elffileheader {
-	u32  magic;
-	u8   class;
-	u8   data;
-	u8   version;
-	u8   osabi;
-	u64  reserved;
-	u16  type;
-	u16  isa;
-	u32  elfversion;
-	uptr peoff;
-	uptr phoff;
-	uptr shoff;
-	u32  flags;
-	u16  ehsize;
-	u16  phesize;
-	u16  phenum;
-	u16  shesize;
-	u16  shenum;
-	u16  shstridx;
+	u32                 magic;
+	u8                  class;
+	u8                  data;
+	u8                  version;
+	u8                  osabi;
+	u64                 reserved;
+	u16                 type;
+	u16                 isa;
+	u32                 elfversion;
+	uptr                peoff;
+	uptr                phoff;
+	uptr                shoff;
+	struct elffileflags flags;
+	u16                 ehsize;
+	u16                 phesize;
+	u16                 phenum;
+	u16                 shesize;
+	u16                 shenum;
+	u16                 shstridx;
 };
 
 struct elfprogheader {
-	u32  stype;
+	u32                 stype;
 #ifdef ELF_BITS_64
-	u32  flags;
+	struct elfprogflags flags;
 #endif
-	uptr soff;
-	uptr svaddr;
-	uptr spaddr;
-	uptr sfsize;
-	uptr smsize;
+	uptr                soff;
+	uptr                svaddr;
+	uptr                spaddr;
+	uptr                sfsize;
+	uptr                smsize;
 #ifdef ELF_BITS_32
-	u32  flags;
+	struct elfprogflags flags;
 #endif
-	uptr alignment;
+	uptr                alignment;
 };
 
 void *
