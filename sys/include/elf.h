f6599b76a199a59053a435e2d1654901067059da
Francesco Saccone <francesco@francescosaccone.com>
2025-09-27 14:16:24 +0200

    feat(sys): return NULL from allocelf in case of error

--- sys/include/elf.h
+++ sys/include/elf.h
@@ -22,7 +22,7 @@ struct elfprogflags {
 };
 
 /* Loads the ELF file whose content is at elf to page table pt and returns its
-   load virtual address. */
+   load virtual address or NULL in case of error. */
 void *allocelf(void *elf, struct elffileflags flags,
                pageentry *pt[PAGE_TABLE_ENTRIES]);
 
