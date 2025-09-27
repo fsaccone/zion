82840b804e9cdfe8892d122fac26e6eddfb9c430
Francesco Saccone <francesco@francescosaccone.com>
2025-09-27 16:34:28 +0200

    fix(sys): save ptable pointer of each level in walkpagetree

--- sys/lib/pagetable.c
+++ sys/lib/pagetable.c
@@ -4,6 +4,11 @@
 #include <arch/types.h>
 #include <pmem.h>
 
+struct walklevel {
+	u32 i;
+	pageentry **ptable;
+};
+
 void
 pagetable(pageentry *ptable[PAGE_TABLE_ENTRIES])
 {
@@ -16,8 +21,8 @@ walkpagetree(pageentry *ptree[PAGE_TABLE_ENTRIES],
              s8 (*check)(pageentry, void *),
              void *extra)
 {
-	pageentry **ptable = ptree;
-	u32 l, lvlidxs[PAGE_TABLE_LEVELS] = { 0 };
+	u32 l;
+	struct walklevel levels[PAGE_TABLE_LEVELS] = { 0 };
 
 	/* Limit maxlvl to PAGE_TABLE_LEVELS - 1. */
 	if (maxlvl > PAGE_TABLE_LEVELS - 1)
@@ -26,12 +31,14 @@ walkpagetree(pageentry *ptree[PAGE_TABLE_ENTRIES],
 	if (minlvl > maxlvl)
 		return NULL;
 
+	levels[0].ptable = ptree;
+
 	for (l = 0; l <= maxlvl; l++) {
-		for (; lvlidxs[l] < PAGE_TABLE_ENTRIES; lvlidxs[l]++) {
+		for (; levels[l].i < PAGE_TABLE_ENTRIES; levels[l].i++) {
 			pageentry *pte;
 
-			pte = (pageentry *)((uptr)ptable
-			                    + lvlidxs[l]
+			pte = (pageentry *)((uptr)levels[l].ptable
+			                    + levels[l].i
 			                      * sizeof(pageentry *));
 
 			/* Only call check if we are at least in level
@@ -47,8 +54,9 @@ walkpagetree(pageentry *ptree[PAGE_TABLE_ENTRIES],
 			}
 
 			if (PAGE_ENTRY_GET_WALKABLE(*pte)) {
-				ptable = (pageentry **)
-				         PAGE_ENTRY_GET_PPN(*pte);
+				levels[l + 1].ptable
+				             = (pageentry **)
+				               PAGE_ENTRY_GET_PPN(*pte);
 				goto nextlevel;
 			}
 		}
