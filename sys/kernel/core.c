115a7d7bd3cfa7145a4983c97d4ea6a68afd409c
Francesco Saccone <francesco@francescosaccone.com>
2025-10-02 13:39:09 +0200

    feat(sys): rewrite panic msg of absence of init.tar

--- sys/kernel/core.c
+++ sys/kernel/core.c
@@ -20,7 +20,7 @@ coremain(u16 c)
 
 	lock(&l);
 	if (!initfiles && !(initfiles = allocinittarfiles())) {
-		setpanicmsg("No init.tar was not found in kernel codespace.");
+		setpanicmsg("Unable to find init.tar in kernel codespace.");
 		tracepanicmsg("coremain");
 		panic();
 	}
