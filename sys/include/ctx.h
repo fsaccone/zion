4016d2ccabe5e91db92c886abff522540b969303
Francesco Saccone <francesco@francescosaccone.com>
2025-10-04 13:57:50 +0200

    feat(sys): define setctxsp

--- sys/include/ctx.h
+++ sys/include/ctx.h
@@ -7,6 +7,9 @@
 /* Sets the program counter of context ctx to pc. */
 void setctxpc(u8 ctx[CTX_SIZE], void *pc);
 
+/* Sets the stack pointer of context ctx to sp. */
+void setctxsp(u8 ctx[CTX_SIZE], void *sp);
+
 /* Make CPU switch to the context in new after saving its current context in
    old. If old is NULL, it just loads new. */
 void switchctx(u8 old[CTX_SIZE], u8 new[CTX_SIZE]);
