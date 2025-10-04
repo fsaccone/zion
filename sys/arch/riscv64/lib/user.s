5b9bf6e00c77b92704453e77cecd67b548eda62e
Francesco Saccone <francesco@francescosaccone.com>
2025-10-04 19:11:52 +0200

    feat(sys): make usermode have no parameter

--- sys/arch/riscv64/lib/user.s
+++ sys/arch/riscv64/lib/user.s
@@ -1,20 +1,26 @@
 .section .text
+.global setuserptree
 .global usermode
 
+setuserptree:
+	la t0, userptree
+	ld a0, 0(t0)
+
+	ret
+
 usermode:
-	csrw sepc, a0
+	csrwi sepc, 0x0
 
-	# Load address at a1.
-	mv t0, a1
-
-	# If address is NULL, just set satp to 0 to use physical memory.
-	beqz t0, 1f
+	# Load page tree address.
+	la t0, userptree
 
 	# Set MODE to 1010 (Sv57).
 	li t1, 0b1010 << 60
 	or t0, t0, t1
 
-1:
 	csrw satp, t0
 
 	sret
+
+.section .data
+userptree: .dword 0
