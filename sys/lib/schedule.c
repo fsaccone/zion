e82446c5cac7ddf6e43c074396405de0ae8028b2
Francesco Saccone <francesco@francescosaccone.com>
2025-10-05 12:10:57 +0200

    feat(sys): implement basic READY scheduling

--- sys/lib/schedule.c
+++ sys/lib/schedule.c
@@ -1,7 +1,29 @@
 #include <schedule.h>
 
+#include <core.h>
+#include <ctx.h>
+#include <machine/cpu.h>
+#include <process.h>
+
+u8 corectxs[NCPU][CTX_SIZE] = { 0 };
+
 void
 schedule(void)
 {
-	for(;;);
+	u16 c = core();
+	struct processnode *pn;
+
+	for(;;) {
+		for (pn = processes(); pn; pn = pn->n) {
+			struct process *p = pn->p;
+
+			switch (p->state) {
+			case READY:
+				p->state = RUNNING;
+				switchctx(corectxs[c], p->ctx);
+				break;
+			default:
+			}
+		}
+	}
 }
