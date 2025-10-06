d94bdf1402ace3076c32b535e689b10f8e0ba7b9
Francesco Saccone <francesco@francescosaccone.com>
2025-10-06 14:26:29 +0200

    feat(sys): redefine raminitbase as raminitframes and use it

--- sys/kernel/raminit.c
+++ sys/kernel/raminit.c
@@ -2,9 +2,47 @@
 
 #include <config.h>
 #include <machine/mem.h>
+#include <panic.h>
+#include <pmem.h>
 
-void *
-raminitbase(void)
+s8
+raminitframes(struct framenode **frames)
 {
-	return (void *)(KERNEL_START + BINARY_SIZE);
+	uptr a, base = KERNEL_START + BINARY_SIZE;
+	struct framenode *tail;
+
+	if (base % PAGE_SIZE) {
+		setpanicmsg("Raminit program not aligned to PAGE_SIZE.");
+		goto panic;
+	}
+
+	if (RAMINIT_BINARY_SIZE % PAGE_SIZE) {
+		setpanicmsg("RAMINIT_BINARY_SIZE not aligned to PAGE_SIZE.");
+		goto panic;
+	}
+
+	*frames = NULL;
+
+	for (a = 0; a < RAMINIT_BINARY_SIZE; a += PAGE_SIZE) {
+		struct framenode *fn;
+
+		if (!(fn = palloc(sizeof(struct framenode), 0)))
+			goto panic;
+
+		fn->f = (void *)(base + a);
+		fn->n = NULL;
+
+		if (!*frames)
+			*frames = fn;
+		else
+			tail->n = fn;
+
+		tail = fn;
+	}
+
+	return 0;
+
+panic:
+	tracepanicmsg("raminitframes");
+	return -1;
 }
--- sys/kernel/raminit.h
+++ sys/kernel/raminit.h
@@ -1,7 +1,11 @@
 #ifndef __RAMINIT_H
 #define __RAMINIT_H
 
-/* Returns the raminint base address. */
-void *raminitbase(void);
+#include <arch/types.h>
+#include <pmem.h>
+
+/* Sets frames linked list to the ordered list of frames containing the raminit
+   program. Returns -1 in case of error or 0 otherwise. */
+s8 raminitframes(struct framenode **frames);
 
 #endif
