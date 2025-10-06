a31f866483aecbbe379736f042e429fc97e49cad
Francesco Saccone <francesco@francescosaccone.com>
2025-10-06 15:43:01 +0200

    fix(sys/riscv): do not load argument regs in switchctx
    Also took the opportunity to not load temporary registers.

--- sys/arch/riscv64/include/arch/ctx.h
+++ sys/arch/riscv64/include/arch/ctx.h
@@ -1,7 +1,8 @@
 #ifndef _ARCH_CTX_H
 #define _ARCH_CTX_H
 
-/* (31 registers * 8 bytes) */
-#define CTX_SIZE (31 * 8)
+/* Registers are (1 ra + 1 sp + 1 gp + 1 tp + 12 * s = 16). Each register is 8
+   bytes. */
+#define CTX_SIZE (16 * 8)
 
 #endif
