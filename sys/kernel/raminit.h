ec799b78c1070f534ca5fa3575eacde224896f53
Francesco Saccone <francesco@francescosaccone.com>
2025-10-06 14:44:23 +0200

    refactor(sys): define and user RAMINIT_BASE in lib/raminit.h

--- sys/kernel/raminit.h
+++ sys/kernel/raminit.h
@@ -2,7 +2,11 @@
 #define __RAMINIT_H
 
 #include <arch/types.h>
+#include <config.h>
+#include <machine/mem.h>
 #include <pmem.h>
+
+#define RAMINIT_BASE (KERNEL_START + BINARY_SIZE)
 
 /* Sets frames linked list to the ordered list of frames containing the raminit
    program. Returns -1 in case of error or 0 otherwise. */
