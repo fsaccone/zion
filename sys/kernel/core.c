8d420ab46b15737e9e0b4e64bf66c7eb85674f96
Francesco Saccone <francesco@francescosaccone.com>
2025-10-06 14:26:29 +0200

    feat(sys): redefine raminitbase as raminitframes and use it

--- sys/kernel/core.c
+++ sys/kernel/core.c
@@ -1,11 +1,16 @@
 #include "core.h"
 
+#include <arch/page.h>
 #include <arch/types.h>
+#include <config.h>
 #include <interrupt.h>
 #include <panic.h>
+#include <process.h>
 #include <schedule.h>
 #include <string.h>
 #include <timer.h>
+
+#include "raminit.h"
 
 /* The main function of core 0, in addition to coremain. Returns -1 in case of
    error or 0 otherwise. */
@@ -14,8 +19,13 @@ static s8 core0(void);
 s8
 core0(void)
 {
-	setpanicmsg("Unimplemented.");
-	goto panic;
+	struct framenode *riframes;
+
+	if (raminitframes(&riframes))
+		goto panic;
+
+	if (createprocess(riframes, NULL))
+		goto panic;
 
 	return 0;
 
