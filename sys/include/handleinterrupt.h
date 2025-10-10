ac5b276393968f825573c27c4215a8ac0f95297d
Francesco Saccone <francesco@francescosaccone.com>
2025-10-10 19:30:07 +0200

    refactor(sys): move handleinterrupt to interrupt.h

--- sys/include/handleinterrupt.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HANDLEINTERRUPT_H
-#define _HANDLEINTERRUPT_H
-
-/* The interrupt handler which is called by the interrupt function, which is
-   the actual entry point of an interrupt. */
-void handleinterrupt(void);
-
-#endif
