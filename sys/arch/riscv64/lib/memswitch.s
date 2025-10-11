52f95bc776bb45f8524c622ffb607c66a00fe421
Francesco Saccone <francesco@francescosaccone.com>
2025-10-11 17:03:39 +0200

    feat(sys): define pmemswitch

--- sys/arch/riscv64/lib/memswitch.s
+++ sys/arch/riscv64/lib/memswitch.s
@@ -1,11 +1,16 @@
 .section .text
 .global memswitch
+.global pmemswitch
 
 memswitch:
-	# Set satp.
 	srli a0,   a0, 12
 	li   t0,   10 << 60
 	or   a0,   a0, t0
 	csrw satp, a0
+
+	ret
+
+pmemswitch:
+	csrwi satp, 0
 
 	ret
