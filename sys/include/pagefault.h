812ac5f64bfe6932becd905252e5b750f5aeddd7
Francesco Saccone <francesco@francescosaccone.com>
2025-10-11 15:34:51 +0200

    feat(sys): define pagefault to use instead of interrupt

---
+++ sys/include/pagefault.h
@@ -0,0 +1,7 @@
+#ifndef _PAGEFAULT_H
+#define _PAGEFAULT_H
+
+/* The page fault handler. */
+void pagefault(void);
+
+#endif
--- sys/lib/interrupt.c
+++ sys/lib/interrupt.c
@@ -1,25 +1,7 @@
 #include <interrupt.h>
 
-#include <arch/types.h>
-#include <panic.h>
-
 void
 interrupt(void)
 {
-	u8 type = interrupttype();
-
-	switch (type) {
-	case INTERRUPT_TYPE_PAGE_FAULT:
-		setpanicmsg("Page fault.");
-		goto panic;
-
-		break;
-	default:
-	}
-
 	return;
-
-panic:
-	tracepanicmsg("handleinterrupt");
-	panic();
 }
