59b007b62bde094c32e22081a8b75fb6a0a1c200
Francesco Saccone <francesco@francescosaccone.com>
2025-10-11 16:14:56 +0200

    feat(sys): pass u argument to exception and pagefault

--- sys/include/exception.h
+++ sys/include/exception.h
@@ -1,7 +1,10 @@
 #ifndef _EXCEPTION_H
 #define _EXCEPTION_H
 
-/* The exception handler. */
-void exception(void);
+#include <arch/types.h>
+
+/* The exception handler. The u argument is 1 when the exception is caused by
+   code in user mode. */
+void exception(u8 u);
 
 #endif
--- sys/include/pagefault.h
+++ sys/include/pagefault.h
@@ -1,7 +1,10 @@
 #ifndef _PAGEFAULT_H
 #define _PAGEFAULT_H
 
-/* The page fault handler. */
-void pagefault(void);
+#include <arch/types.h>
+
+/* The page fault handler. The u argument is 1 when the page fault is caused by
+   code in user mode. */
+void pagefault(u8 u);
 
 #endif
