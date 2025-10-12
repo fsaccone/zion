a5785b75f010780660683fe93f184fbecc9112bc
Francesco Saccone <francesco@francescosaccone.com>
2025-10-12 12:28:35 +0200

    fix(sys/riscv): flush tlb before and after writing to satp

--- sys/arch/riscv64/lib/memswitch.s
+++ sys/arch/riscv64/lib/memswitch.s
@@ -3,14 +3,22 @@
 .global pmemswitch
 
 memswitch:
+	sfence.vma zero, zero
+
 	srli a0,   a0, 12
 	li   t0,   10 << 60
 	or   a0,   a0, t0
 	csrw satp, a0
 
+	sfence.vma zero, zero
+
 	ret
 
 pmemswitch:
+	sfence.vma zero, zero
+
 	csrwi satp, 0
+
+	sfence.vma zero, zero
 
 	ret
