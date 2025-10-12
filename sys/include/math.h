f85ff96eaa67b0eab0cdeefcd99e71c737d01ba8
Francesco Saccone <francesco@francescosaccone.com>
2025-10-12 16:16:09 +0200

    chore(sys): comment FLOOR and CEIL

--- sys/include/math.h
+++ sys/include/math.h
@@ -1,6 +1,7 @@
 #ifndef _MATH_H
 #define _MATH_H
 
+/* Round integer x to its previous/next multiple of n. */
 #define FLOOR(x, n) (((x) / (n)) * (n))
 #define CEIL(x, n)  ((((x) + (n) - 1) / (n)) * (n))
 
