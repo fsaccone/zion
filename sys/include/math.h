e1dad08a792ab42b3faf69992ef7aba764066f48
Francesco Saccone <francesco@francescosaccone.com>
2025-10-12 16:13:53 +0200

    feat(sys): define FLOOR in math.h

--- sys/include/math.h
+++ sys/include/math.h
@@ -1,7 +1,8 @@
 #ifndef _MATH_H
 #define _MATH_H
 
-#define CEIL(x, n) ((((x) + (n) - 1) / (n)) * (n))
+#define FLOOR(x, n) (((x) / (n)) * (n))
+#define CEIL(x, n)  ((((x) + (n) - 1) / (n)) * (n))
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
