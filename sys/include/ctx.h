cfb3ca3c23b8c593c76c40b4339c557f01fd4bf0
Francesco Saccone <francesco@francescosaccone.com>
2025-10-12 10:43:52 +0200

    refactor(sys): remove unused ctx functions

--- sys/include/ctx.h
+++ sys/include/ctx.h
@@ -4,20 +4,8 @@
 #include <arch/ctx.h>
 #include <arch/types.h>
 
-/* Returns the program counter of context ctx. */
-void *getctxpc(u8 ctx[CTX_SIZE]);
-
-/* Returns the stack pointer of context ctx. */
-void *getctxsp(u8 ctx[CTX_SIZE]);
-
-/* Returns the stack pointer of the running core. */
-void *getcursp(void);
-
 /* Sets the program counter of context ctx to pc. */
 void setctxpc(u8 ctx[CTX_SIZE], void *pc);
-
-/* Sets the stack pointer of context ctx to sp. */
-void setctxsp(u8 ctx[CTX_SIZE], void *sp);
 
 /* Make CPU switch to the context in new after saving its current context in
    old. */
