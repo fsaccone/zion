dd4bc8900d46c8ee61d350091a4955b84f3c0d99
Francesco Saccone <francesco@francescosaccone.com>
2025-10-13 13:30:27 +0200

    feat(sys/raminit): allocate stack and call raminit

--- sys/raminit/arch/riscv64/start.s
+++ sys/raminit/arch/riscv64/start.s
@@ -2,5 +2,23 @@
 .global start
 
 start:
+	call allocstack
+	mv   sp, a0
+
+	call raminit
+
 1:
 	j 1b
+
+# Allocates stack and returns its end address.
+allocstack:
+	li a0, 0
+	li a1, 8192
+	ecall
+
+	# Since stack is 2 pages, add (2 * PAGE_SIZE - 1) to the first address
+	# to get the end address.
+	li  t0, 8192 - 1
+	add a0, a0, t0
+
+	ret
